b9547c071075bdcf599998c916eb0bc9a5d1abab
korol <cx.korol@gmail.com>
2019-03-19 17:37:02 +0300

    Padawans task: money-exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let result = {};
+
+        if (currency <= 0) {
+            return result;
+        }
+        if (currency >= 10000) {
+            return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+        } else {
+            let half = Math.floor(currency / 50);
+            currency -= 50 * half;
+            let quarter = Math.floor(currency / 25);
+            currency -= 25 * quarter;
+            let dime = Math.floor(currency / 10);
+            currency -= 10 * dime;
+            let nickel = Math.floor(currency / 5); 
+            currency -= 5 * nickel;
+            let penny = currency;
+            
+            if (half > 0) {
+                result.H = half;
+            }
+            if (quarter > 0) {
+                result.Q = quarter;
+            }
+            if (dime > 0) {
+                result.D = dime;
+            }
+            if (nickel > 0) {
+                result.N = nickel;
+            }
+            if (penny > 0) {
+                result.P = penny;
+            }
+
+            return result;
+            }
 }
